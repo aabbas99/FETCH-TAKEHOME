afbc82a5402315e79122485b1b7a33ed982a3852
aabbas99 <aabbas99@terpmail.umd.edu>
2024-03-18 16:55:05 -0400

    Initial Commit

---
+++ FETCH TAKEHOME/FETCH_TAKEHOMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  FETCH_TAKEHOMEApp.swift
+//  FETCH TAKEHOME
+//
+//  Created by Abdul’s IPhone on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FETCH_TAKEHOMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FETCH TAKEHOMEUITests/FETCH_TAKEHOMEUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FETCH_TAKEHOMEUITestsLaunchTests.swift
+//  FETCH TAKEHOMEUITests
+//
+//  Created by Abdul’s IPhone on 3/18/24.
+//
+
+import XCTest
+
+final class FETCH_TAKEHOMEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
